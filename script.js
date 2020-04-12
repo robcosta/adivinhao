fa22578bc8a41fe6357d644921b1bfbd37b37805
Roberto Costa <robertocosta66@gmail.com>
2020-04-12 20:02:20 -0300

    Input functionality only numbers and a maximum of two digits

--- script.js
+++ script.js
@@ -65,3 +65,22 @@ function teclouEnter(event){
         }
     }
 }
+
+//Aceita somente numeros e bloqueia mais de dois dígitos
+function doisDigitos(e){
+    var valor = document.getElementById('palpite').value;
+    var charCode = e.charCode ? e.charCode : e.keyCode;
+    // charCode 8 = backspace   
+    // charCode 9 = tab
+    if (charCode != 8 && charCode != 9) {
+        // charCode 48 equivale a 0   
+        // charCode 57 equivale a 9
+        if (charCode < 48 || charCode > 57) {
+            return false;
+        }
+    }
+    if(valor.length >= 2){
+        return false;
+    }
+    return true;
+}
